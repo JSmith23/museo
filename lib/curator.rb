d595ac89b38b65f997d024d74a49fcd441232e99
Jimmy Smith <jaystansmith@gmail.com>
2018-09-18 10:46:01 -0600

    Added non function test suites

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,5 @@
+require './lib/photograph'
+
 class Curator
   attr_reader :artists,
               :photographs
@@ -5,5 +7,26 @@ class Curator
   def initialize
     @artists = []
     @photographs = []
+  end
+
+  def add_photograph(photograph_attributes)
+    @photographs << Photograph.new(photograph_attributes)
+  end
+
+  def add_artist(artist_attributes)
+    @artists << Artist.new(artist_attributes)
+  end
+
+  def find_artist_by_id(id)
+    @artists.find_all do |artist|
+      holder = artist
+      artist.id == id
+    end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find_all do |photograph|
+      photograph.id == id
+    end
   end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -3,21 +3,151 @@ require 'minitest/pride'
 require './lib/file_io'
 require './lib/artist'
 require './lib/curator'
+require './lib/photograph'
+require 'pry'
 
 class CuratorTest < MiniTest::Test
   def test_it_exists
+    skip
     curator = Curator.new
     assert_instance_of Curator, curator
   end
 
   def test_it_can_add_photographs
+    skip
     photo_1 = {
   id: "1",
   name: "Rue Mouffetard, Paris (Boy with Bottles)",
   artist_id: "1",
   year: "1954"
   }
+
+  photo_2 = {
+  id: "2",
+  name: "Moonrise, Hernandez",
+  artist_id: "2",
+  year: "1941"
+  }
+
   curator = Curator.new
-   
+  curator.add_photograph(photo_1)
+  diff [Photograph.new(photo_1)],curator.photographs
+  curator.add_photograph(photo_2)
+  diff [Photograph.new(photo_1),Photograph.new(photo_2)],curator.photographs
   end
+
+  def test_first_returns_first_photo_object_in_array
+    skip
+    photo_1 = {
+  id: "1",
+  name: "Rue Mouffetard, Paris (Boy with Bottles)",
+  artist_id: "1",
+  year: "1954"
+  }
+
+  photo_2 = {
+  id: "2",
+  name: "Moonrise, Hernandez",
+  artist_id: "2",
+  year: "1941"
+  }
+
+  curator = Curator.new
+  curator.add_photograph(photo_1)
+  curator.add_photograph(photo_2)
+  assert_equal Photograph.new(photo_1), curator.photographs.first
+
+  end
+
+  def test_it_can_add_artists
+    skip
+    artist_1 = {
+  id: "1",
+  name: "Henri Cartier-Bresson",
+  born: "1908",
+  died: "2004",
+  country: "France"
+  }
+
+  artist_2 = {
+  id: "2",
+  name: "Ansel Adams",
+  born: "1902",
+  died: "1984",
+  country: "United States"
+  }
+
+  curator = Curator.new
+  curator.add_artist(artist_1)
+  assert_equal [holder_1], curator.artists
+  end
+
+  def test_artist_can_return_its_attributes
+    artist_1 = {
+  id: "1",
+  name: "Henri Cartier-Bresson",
+  born: "1908",
+  died: "2004",
+  country: "France"
+  }
+
+  artist_2 = {
+  id: "2",
+  name: "Ansel Adams",
+  born: "1902",
+  died: "1984",
+  country: "United States"
+  }
+
+  curator = Curator.new
+  curator.add_artist(artist_1)
+  curator.add_artist(artist_2)
+  assert_equal "Henri Cartier-Bresson", curator.artists.first.name
+  end
+
+  def test_find_artist_by_id
+    artist_1 = {
+  id: "1",
+  name: "Henri Cartier-Bresson",
+  born: "1908",
+  died: "2004",
+  country: "France"
+  }
+
+  artist_2 = {
+  id: "2",
+  name: "Ansel Adams",
+  born: "1902",
+  died: "1984",
+  country: "United States"
+  }
+
+  curator = Curator.new
+  curator.add_artist(artist_1)
+  curator.add_artist(artist_2)
+  assert_equal Artist.new(artist_1), curator.find_artist_by_id("1").first
+
+  end
+
+  def test_it_can_find_photograph_by_id
+    photo_1 = {
+  id: "1",
+  name: "Rue Mouffetard, Paris (Boy with Bottles)",
+  artist_id: "1",
+  year: "1954"
+  }
+
+  photo_2 = {
+  id: "2",
+  name: "Moonrise, Hernandez",
+  artist_id: "2",
+  year: "1941"
+  }
+
+  curator = Curator.new
+  curator.add_photograph(photo_1)
+  curator.add_photograph(photo_2)
+  assert_equal Photograph.new(photo_2), curator.find_photograph_by_id("2").first
+  end
+
 end
